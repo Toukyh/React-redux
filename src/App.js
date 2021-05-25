be927bc5957ff7a0da2752c810fdda0bceb5e5cf
Warthog <warthogtouky@gmail.com>
2021-05-25 11:21:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className="text-center">
+          <a className="bg-indigo-500 py-2 px-2 text-gray-100 rounded-xl hover:bg-indigo-600" href="#">Hello world</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
